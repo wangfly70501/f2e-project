06caa39168f7d5180a81cb2c79bc315bc621dd13
josh_wang <josh_wang@btcbox.com.tw>
2020-02-21 14:42:06 +0800

    [修改]excel

--- src/json2excel.js
+++ src/json2excel.js
@@ -10,15 +10,15 @@ function datenum (v, date1904) {
 function data2ws (data) {
   const ws = {}
   const range = { s: { c: 10000000, r: 10000000 }, e: { c: 0, r: 0 } }
-  for (let R = 0; R != data.length; ++R) {
-    for (let C = 0; C != data[R].length; ++C) {
+  for (let R = 0; R !== data.length; ++R) {
+    for (let C = 0; C !== data[R].length; ++C) {
       if (range.s.r > R) range.s.r = R
       if (range.s.c > C) range.s.c = C
       if (range.e.r < R) range.e.r = R
       if (range.e.c < C) range.e.c = C
       const cell = { v: data[R][C] }
       if (cell.v == null) continue
-      const cell_ref = XLSX.utils.encode_cell({ c: C, r: R })
+      const cellRef = XLSX.utils.encode_cell({ c: C, r: R })
 
       if (typeof cell.v === 'number') cell.t = 'n'
       else if (typeof cell.v === 'boolean') cell.t = 'b'
@@ -28,7 +28,7 @@ function data2ws (data) {
         cell.v = datenum(cell.v)
       } else cell.t = 's'
 
-      ws[cell_ref] = cell
+      ws[cellRef] = cell
     }
   }
   if (range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range)
@@ -43,7 +43,7 @@ function Workbook () {
 function s2ab (s) {
   const buf = new ArrayBuffer(s.length)
   const view = new Uint8Array(buf)
-  for (let i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF
+  for (let i = 0; i !== s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF
   return buf
 }
 
