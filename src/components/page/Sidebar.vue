bf09aca8f07f346f0bf5ad0eec965a91a40d0157
josh_wang <josh_wang@btcbox.com.tw>
2020-02-27 10:04:51 +0800

    [修改]sidebar

--- src/components/page/Sidebar.vue
+++ src/components/page/Sidebar.vue
@@ -127,7 +127,7 @@ export default {
           title: '項目管理',
           subs: [
             { index: 'realname', title: '實名認證' },
-            { index: 'lockup', title: '鎖倉管理' },
+            { index: 'lockup', title: '鎖倉管理' }
             /*  { index: 'test', title: '測試' } */
           ]
         }
